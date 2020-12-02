torques and forces based dynamics and changes state to include angular velocity, fixed angular acceleration derivative

example.py
@@ -27,9 +27,9 @@ magnetorquer_z = components.SimpleSolenoidMagnetorquer(position=(0, 0, 0), orien
                                                        cubesat=qubesat, number_of_loops=10, area=0.5)
 
 qubesat.add_sensor(lsm9ds1)
-qubesat.add_actuator(magnetorquer_x)
-qubesat.add_actuator(magnetorquer_y)
-qubesat.add_actuator(magnetorquer_z)
+# qubesat.add_actuator(magnetorquer_x)
+# qubesat.add_actuator(magnetorquer_y)
+# qubesat.add_actuator(magnetorquer_z)
 
 # dipole in tesla referenced from the cubesat frame
 qubesat.add_static_magnetic_dipole(np.array([0, 0, 0]))
@@ -42,18 +42,18 @@ Step 2: Configure the initial state of the cubesat in the simulation
 """
 inclination = constants.ISS_INCLINATION
 altitude = constants.ISS_ALTITUDE
-max_step_size = 1
+max_step_size = 100
 
 print("Starting inclination:", str(np.degrees(inclination)) + "deg", "\nStarting altitude:", str(altitude) + "m", '\n')
 
 v_init = utils.inclination_to_cartesian_velocity(utils.circular_orbit_velocity(altitude), inclination)
 
 # roll, pitch, yaw are defined referenced to ecef, droll, dpitch, dyaw are body rates
-# r, p, y and dr, dp, dyaw are converted to quaternion with the following two functions
+# r, p, y is converted to quaternion with the following functions. pqr are angular velocities
 q_init = utils.euler_to_quaternion(0, 0, 0)
-dq_init = utils.angular_velocity_to_dquaternion([0.001, 0.0, 0.0], q_init)
+pqr = [0.001, 0.0, 0.001]
 
-initial_state = state.State(altitude + constants.R_EARTH, 0, 0, *v_init, *q_init, *dq_init)
+initial_state = state.State(altitude + constants.R_EARTH, 0, 0, *v_init, *q_init, *pqr)
 simulator = physics.Simulator(qubesat, planet, initial_state, max_step_size)
 
 """

simple_space_simulator/cubesat.py
@@ -139,28 +139,27 @@ class Cubesat:
         self.internal_state_history.append(internal_state)
         control_commands = self.controller(time, internal_state, sensor_readings)
 
-        internal_acc, internal_angular_acc = np.zeros(3), np.zeros(3)
+        internal_force, internal_torque = np.zeros(3), np.zeros(3)
         magnetic_dipole = self.get_static_magnetic_dipole()
         for actuator in self.actuators:
             if control_commands is not None and actuator.id in control_commands:
                 command = control_commands[actuator.id]
                 actuator_force, actuator_torque, actuator_dipole = actuator.command(time, command)
-                internal_acc += actuator_force / self.mass
-                internal_angular_acc += np.dot(self.inertia_inv, actuator_torque)
+                internal_force += actuator_force
+                internal_torque += actuator_torque
                 magnetic_dipole += actuator_dipole
 
         # incorporate torque due to magnetic field
-        internal_angular_acc += np.dot(self.inertia_inv,
-                                       np.cross(utils.quaternion_rotate(
-                                           external_state.get_orientation_quaternion(), magnetic_dipole),
-                                        external_magnetic_field))
+        internal_torque += np.cross(utils.quaternion_rotate(external_state.get_orientation_quaternion(),
+                                                            magnetic_dipole),
+                                    external_magnetic_field)
 
-        self.control_history.append({'linear acc': internal_acc,
-                                     'angular acc': internal_angular_acc,
+        self.control_history.append({'force': internal_force,
+                                     'torque': internal_torque,
                                      'magnetic dipole': magnetic_dipole,
                                      'actuator commands': control_commands})
 
-        return internal_acc, internal_angular_acc
+        return internal_force, internal_torque
 
     def reset(self):
         self.internal_state_history = []

simple_space_simulator/physics.py
@@ -55,11 +55,8 @@ class Simulator:
         # function that takes in a state and returns a torque vector <tx, ty, tz> acting around the COM of the
         # cubesat with inertia I
         self.torques = []
-        # function that takes in a state and returns an angular acceleration vector <alphax, alphay, alphaz> acting
-        # around the COM
-        self.angular_accelerations = []
 
-    def compute_lateral_accelerations(self, s):
+    def compute_external_force(self, s):
 
         net_force = np.zeros(3)
         for forcer in self.forces:
@@ -69,23 +66,17 @@ class Simulator:
         for accelerator in self.accelerations:
             net_acc += accelerator(s, self.cubesat, self.planet)
 
-        net_acc += net_force / self.cubesat.mass
+        net_force += net_acc * self.cubesat.mass
 
-        return net_acc
+        return net_force
 
-    def compute_angular_accelerations(self, s):
+    def compute_external_torque(self, s):
 
         net_torque = np.zeros(3)
         for torquer in self.torques:
             net_torque += torquer(s, self.cubesat, self.planet)
 
-        net_angular_acc = np.zeros(3)
-        for angular_accelerator in self.angular_accelerations:
-            net_angular_acc += angular_accelerator(s, self.cubesat, self.planet)
-
-        net_angular_acc += np.dot(self.cubesat.inertia_inv, net_torque)
-
-        return net_angular_acc
+        return net_torque
 
     def sample(self, t, y):
         """
@@ -100,28 +91,29 @@ class Simulator:
 
         external_state = state_from_vector(y)
 
-        # 1. Compute the external accelerations
-        external_acc = self.compute_lateral_accelerations(external_state)
-        external_angular_acc = self.compute_angular_accelerations(external_state)
+        # 1. Compute the external forces acting on the cubesat
+        external_force = self.compute_external_force(external_state)
+        external_torque = self.compute_external_torque(external_state)
 
-        # 2. Compute the internal / commanded accelerations
-        internal_acc, internal_angular_acc = self.cubesat(t, external_state, external_acc, external_angular_acc,
-                                                          self.planet.get_magnetic_field(external_state))
+        # 2. Compute the internal / commanded forces / torques
+        internal_force, internal_torque = self.cubesat(t, external_state, external_force, external_torque,
+                                                       self.planet.get_magnetic_field(external_state))
 
-        # 3. Sum external and internal accelerations
-        total_acc, total_angular_acc = external_acc + internal_acc, external_angular_acc + internal_angular_acc
+        # 3. Sum external and internal forces / torques
+        net_force, net_torque = external_force + internal_force, external_torque + internal_torque
 
-        # 4. Quaternion computations for integrating orientation
-        # see experimentation/dynamics_modeling.ipynb for resources on quaternions and integrating orientation
-        dquat = external_state.get_quaternion_derivative()
-        ddquat = 1 / 2 * (utils.quaternion_multiply(dquat, [0, *external_state.get_angular_velocity_vector()]) +
-                          utils.quaternion_multiply(external_state.get_orientation_quaternion(),
-                                                    [0, *total_angular_acc]))
+        # 4. Get the net acceleration
+        net_acc = net_force / self.cubesat.mass
+
+        # 5. Quaternion computations for integrating orientation and find the net angular acceleration in body frame
+        angular_velocity = external_state.get_angular_velocity_vector()
+        angular_momentum = np.dot(self.cubesat.inertia, angular_velocity)
+        angular_acc = np.dot(self.cubesat.inertia_inv, (net_torque - np.cross(angular_velocity, angular_momentum)))
 
         return (*external_state.get_velocity_vector(),
-                *total_acc,
-                *dquat,
-                *ddquat)
+                *net_acc,
+                *external_state.get_quaternion_derivative(),
+                *angular_acc)
 
     def run(self, start=0, stop=5000, sample_resolution=10):
         """
@@ -162,10 +154,6 @@ class Simulator:
     def add_torquer(self, torquer):
         assert callable(torquer), "torquer must be a function"
         self.torques.append(torquer)
-
-    def add_angular_accelerator(self, angular_accelerator):
-        assert callable(angular_accelerator), "angular_accelerator must be a function"
-        self.angular_accelerations.append(angular_accelerator)
 
 
 class Planet:

simple_space_simulator/state.py
@@ -7,7 +7,7 @@ import simple_space_simulator.physics as physics
 
 
 class State:
-    def __init__(self, x, y, z, dx, dy, dz, qw, qx, qy, qz, dqw, dqx, dqy, dqz):
+    def __init__(self, x, y, z, dx, dy, dz, qw, qx, qy, qz, p, q, r):
         """
         Structure of the external state vector (units are m, m/s, radians, radians/s)
         x, y, z, dx, dy, dz, roll, pitch, yaw, droll, dpitch, dyaw
@@ -24,7 +24,7 @@ class State:
                isinstance(qy, (int, float)) and isinstance(qz, (int, float)), \
                "orientation derivative must contain integers or floats"
 
-        self.state_vector = np.array([x, y, z, dx, dy, dz, qw, qx, qy, qz, dqw, dqx, dqy, dqz])
+        self.state_vector = np.array([x, y, z, dx, dy, dz, qw, qx, qy, qz, p, q, r])
 
     def get_cartesian_state_vector(self):
         """
@@ -50,14 +50,18 @@ class State:
         return utils.quaternion_conjugate(self.get_orientation_quaternion())
 
     def get_quaternion_derivative(self):
-        return self.state_vector[10:14]
+        p, q, r = self.get_angular_velocity_vector()
+        pqr_mat = np.array([[0, -p, -q, -r],
+                            [p, 0, r, -q],
+                            [q, -r, 0, p],
+                            [r, q, -p, 0]])
+        return 1/2 * np.dot(pqr_mat, self.get_orientation_quaternion())
 
     def get_velocity_vector(self):
         return self.state_vector[3:6]
 
     def get_angular_velocity_vector(self):
-        return utils.quaternion_multiply(self.get_orientation_quaternion_conjugate(),
-                                         2 * self.get_quaternion_derivative())[1:]
+        return self.state_vector[10:13]
 
     def get_spherical_state_vector(self):
         """
@@ -171,4 +175,4 @@ class State:
 
 def state_from_vector(v):
     # v is [x, y, z, dx, dy, dz, r, p y, wr, wp, wy]
-    return State(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12], v[13])
+    return State(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], v[10], v[11], v[12])